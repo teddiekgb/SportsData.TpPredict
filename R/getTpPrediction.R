acea1f5cc3fa1048ff858c00c1a7ec1abb77cb38
teddiekgb <teddiekgb2345@hotmail.com>
2019-11-29 13:31:11 +0800

    Changed states to Shirley structure

--- R/getTpPrediction.R
+++ R/getTpPrediction.R
@@ -12,48 +12,10 @@ getTpPrediction <- function(AWAY_EFG, AWAY_TOVr, AWAY_OREBr, AWAY_DREBr, AWAY_FT
                                   "HOME_oFTF" = HOME_oFTF))
 
   # declare Markov-chain states
-  states_temp <- c("HOME_JUMP_BALL", "AWAY_JUMP_BALL",
-                   "HOME_TURNOVER_DEAD", "AWAY_TURNOVER_DEAD",
-                   "HOME_TURNOVER_LIVE", "AWAY_TURNOVER_LIVE",
-                   "HOME_FOULED_NON_SHOOTING", "AWAY_FOULED_NON_SHOOTING",
-                   "HOME_FOULED_INBOUNDS_LAST_2_MINS", "AWAY_FOULED_INBOUNDS_LAST_2_MINS",
-                   "HOME_FOULED_INBOUNDS", "AWAY_FOULED_INBOUNDS",
-                   "HOME_FOULED_CLEAR_PATH", "AWAY_FOULED_CLEAR_PATH",
-                   "HOME_FOULED_FLAGRANT", "AWAY_FOULED_FLAGRANT",
-                   "HOME_FOULED_TECHNICAL", "AWAY_FOULED_TECHNICAL",
-                   "HOME_FT_WITH_POSSESSION_1_OF_1_MADE", "AWAY_FT_WITH_POSSESSION_1_OF_1_MADE",
-                   "HOME_FT_WITH_POSSESSION_1_OF_1_MISSED", "AWAY_FT_WITH_POSSESSION_1_OF_1_MISSED",
-                   "HOME_FT_WITH_POSSESSION_1_OF_2_MADE", "AWAY_FT_WITH_POSSESSION_1_OF_2_MADE",
-                   "HOME_FT_WITH_POSSESSION_1_OF_2_MISSED", "AWAY_FT_WITH_POSSESSION_1_OF_2_MISSED",
-                   "HOME_FT_WITH_POSSESSION_2_OF_2_MADE", "AWAY_FT_WITH_POSSESSION_2_OF_2_MADE",
-                   "HOME_FT_WITH_POSSESSION_2_OF_2_MISSED", "AWAY_FT_WITH_POSSESSION_2_OF_2_MISSED",
-                   "HOME_FT_1_OF_1_MADE", "AWAY_FT_1_OF_1_MADE",
-                   "HOME_FT_1_OF_1_MISSED", "AWAY_FT_1_OF_1_MISSED",
-                   "HOME_FT_1_OF_2_MADE", "AWAY_FT_1_OF_2_MADE",
-                   "HOME_FT_1_OF_2_MISSED", "AWAY_FT_1_OF_2_MISSED",
-                   "HOME_FT_2_OF_2_MADE", "AWAY_FT_2_OF_2_MADE",
-                   "HOME_FT_2_OF_2_MISSED", "AWAY_FT_2_OF_2_MISSED",
-                   "HOME_FT_1_OF_3_MADE", "AWAY_FT_1_OF_3_MADE",
-                   "HOME_FT_1_OF_3_MISSED", "AWAY_FT_1_OF_3_MISSED",
-                   "HOME_FT_2_OF_3_MADE", "AWAY_FT_2_OF_3_MADE",
-                   "HOME_FT_2_OF_3_MISSED", "AWAY_FT_2_OF_3_MISSED",
-                   "HOME_FT_3_OF_3_MADE", "AWAY_FT_3_OF_3_MADE",
-                   "HOME_FT_3_OF_3_MISSED", "AWAY_FT_3_OF_3_MISSED",
-                   "HOME_DREB", "AWAY_DREB",
-                   "HOME_OREB", "AWAY_OREB",
-                   "HOME_2PT_FG_MADE_AND_FOUL", "AWAY_2PT_FG_MADE_AND_FOUL",
-                   "HOME_2PT_FG_MADE", "AWAY_2PT_FG_MADE",
-                   "HOME_2PT_FG_MISSED_AND_FOUL", "AWAY_2PT_FG_MISSED_AND_FOUL",
-                   "HOME_2PT_FG_MISSED", "AWAY_2PT_FG_MISSED",
-                   "HOME_3PT_FG_MADE_AND_FOUL", "AWAY_3PT_FG_MADE_AND_FOUL",
-                   "HOME_3PT_FG_MADE", "AWAY_3PT_FG_MADE",
-                   "HOME_3PT_FG_MISSED_AND_FOUL", "AWAY_3PT_FG_MISSED_AND_FOUL",
-                   "HOME_3PT_FG_MISSED", "AWAY_3PT_FG_MISSED",
-                   "HOME_TIMEOUT", "AWAY_TIMEOUT",
-                   "HOME_KICKED_BALL", "AWAY_KICKED_BALL")
+  states_temp <- c("Ai0", "Ai1", "Ai2", "Ai3", "As0", "Af0", "Af1", "Af2", "Af3", "Ad0", "Ad1", "Ad2", "Ao0", "Ao1", "Ao2", "Bi0", "Bi1", "Bi2", "Bi3", "Bs0", "Bf0", "Bf1", "Bf2", "Bf3", "Bd0", "Bd1", "Bd2", "Bo0", "Bo1", "Bo2")
 
   # create skeleton transition matrix
-  m <- matrix(0.0000, nrow = 78, ncol = 78)
+  m <- matrix(0.0000, nrow = 30, ncol = 30)
   colnames(m) <- states_temp
   rownames(m) <- states_temp
 
